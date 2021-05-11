6cd9a65593004d789dee42ebe6d9069cf1d57ff4
magentix <mvion@magentix.fr>
2021-05-11 14:41:08 +0200

    Analytic script updated

--- source/web/js/app.js
+++ source/web/js/app.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var element = document.createElement('img');
-    element.src = 'https://cdn.magentix.fr/magentix.png';
-    element.alt = '';
-    document.body.appendChild(element);
-}, false);
+document.addEventListener("DOMContentLoaded",function(){
+    var e=window.location.pathname.replace(/^\/|\/$/g,""),n=document.createElement("img");
+    n.src="https://cdn.magentix.fr/mgx-ack-"+encodeURI(e||"root")+".png";
+    n.alt="";
+    document.body.appendChild(n)
+},!1);
