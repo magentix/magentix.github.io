fc602bbeda1602b81496b2a03f079cde8222482c
magentix <mvion@magentix.fr>
2021-10-22 14:39:05 +0200

    New article

--- web/vercel/js/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){
-    var l=window.location,e=(l.pathname+encodeURIComponent(l.search)).replace(/^\/|\/$/g,"");
-    var n=document.createElement("img");
-    n.src="https://cdn.magentix.fr/mgx-ack-"+encodeURI(e||"root")+".png";
-    n.style.display = "none";
-    n.alt="";
-    document.body.appendChild(n);
-},!1);
